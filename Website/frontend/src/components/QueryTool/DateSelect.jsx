b2e94c7fdc81e5995870033f0735c3c3b9bc0c67
Gabriel Sessions <gsess7732@gmail.com>
2023-04-18 14:08:40 -0400

    DateSelect Modularization

--- Website/frontend/src/components/QueryTool/DateSelect.jsx
+++ Website/frontend/src/components/QueryTool/DateSelect.jsx
@@ -1,4 +1,4 @@
-import { TextInput, Label } from "flowbite-react";
+import TextInputFilter from "./TextInputFilter";
 
 /**
  * DateSelect Component
@@ -12,39 +12,22 @@ export default function DateSelect(props) {
       <div className="flex flex-col lg:ml-12">
         <div className="text-center">
           <div className="my-4">
-            <div className="mb-2">
-              <Label
-                htmlFor="startDate"
-                value="Start Date"
-                className=""
-              />
-            </div>
-            <TextInput
-              id="startDate"
-              type="text"
-              sizing="md"
+            <TextInputFilter
+              label={"Start Date"}
               value={props.query.daySelect[0]}
               onChange={(e) => props.setQuery((prev) => {
-                const copy = {...prev}
+                const copy = { ...prev }
                 copy.daySelect[0] = e.target.value;
                 return copy;
               })}
             />
           </div>
           <div className="my-4">
-            <div className="mb-2 block">
-              <Label
-                htmlFor="endDate"
-                value="End Date"
-              />
-            </div>
-            <TextInput
-              id="endDate"
-              type="text"
-              sizing="md"
+            <TextInputFilter
+              label={"End Date"}
               value={props.query.daySelect[1]}
               onChange={(e) => props.setQuery((prev) => {
-                const copy = {...prev}
+                const copy = { ...prev }
                 copy.daySelect[1] = e.target.value;
                 return copy;
               })}
