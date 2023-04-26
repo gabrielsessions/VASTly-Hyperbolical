df402d57bd19eaa54cc8e3e412345f0f0dbe0196
Gabriel Sessions <gsess7732@gmail.com>
2023-04-26 15:13:09 -0400

    Quick Scatterplot bug fix (was trying stuff out earlier))

--- Website/frontend/src/components/AnalyticsSystem/VehicleMap.jsx
+++ Website/frontend/src/components/AnalyticsSystem/VehicleMap.jsx
@@ -1,7 +1,4 @@
 // Import Stuff Here!
-import { useD3 } from "../../useD3";
-import * as d3 from 'd3';
-import Scatterplot from "./ScatterPlot";
 
 const data = [{x: [1, 3, 4, 5], y: [3, 12, 1, 4], type: 'line'}];
 
