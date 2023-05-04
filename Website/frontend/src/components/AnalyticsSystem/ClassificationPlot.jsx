0c55b77fd22910a9610f6d0c5714a62b5845f347
jchill-git <66270164+jchill-git@users.noreply.github.com>
2023-05-04 18:36:02 -0400

    Colorful checks!

--- Website/frontend/src/components/AnalyticsSystem/ClassificationPlot.jsx
+++ Website/frontend/src/components/AnalyticsSystem/ClassificationPlot.jsx
@@ -33,6 +33,35 @@ export default function ClassificationPlot(props) {
     return [xMin,xMax,yMin,yMax]
 }
 
+var myColor = scaleSequential()
+.domain([-1,18])
+.interpolator(interpolateRainbow);
+
+function itemToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function colorRecode(color){
+  function rgbToHex(rgb) {
+    // Convert each RGB component to its corresponding hex value
+    const r = rgb[0].toString(16).padStart(2, "0");
+    const g = rgb[1].toString(16).padStart(2, "0");
+    const b = rgb[2].toString(16).padStart(2, "0");
+    
+    // Concatenate the hex values to form the final hex code
+    const hexCode = `#${r}${g}${b}`;
+    
+    return hexCode;
+  }
+
+  const integerRegex = /\d+/g; // Match one or more digits
+  const matches = color.match(integerRegex); // Find all matches of the regex in the string
+  const integers = matches.map(Number); // Convert each match to a number
+  
+  return rgbToHex(integers);
+}
+
   useEffect(() => {
     const svg = select(svgRef.current);
     const data = props.TSNEQuery.data;
@@ -49,11 +78,6 @@ export default function ClassificationPlot(props) {
     const yScale = scaleLinear()
       .domain([axesLimits[2]*1.05, axesLimits[3]*1.05])
       .range([dimensions.height, 0]);
-
-
-    var myColor = scaleSequential()
-    .domain([-1,18])
-    .interpolator(interpolateRainbow);
 
     var determineDotSize = scaleLinear()
       .domain([0, 18708])
@@ -106,7 +130,7 @@ export default function ClassificationPlot(props) {
       .selectAll(".dot")
       .data(data)
       .join(
-        enter =>{console.log(data);console.log(determineDotSize(data.length));
+        enter =>{console.log("enter");console.log(colorRecode(myColor(-1)));
         return enter.append("path")
           .attr("class", "dot")
           .attr("opacity", determineDotOpacity(data.length))
@@ -147,10 +171,30 @@ return (
 
     <div className="scatterplot">
       <div className = "legend" id="clusters">
-        <label><input type="checkbox" value="-1" className="w-4 h-4 text-[ ${myColor(-1) }] bg-[ ${myColor(-1) }] border-gray-300 rounded focus:ring-0 focus:ring-offset-0"/>unclustered</label><br />
-        <label><input type="checkbox" value="0" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-0 focus:ring-offset-0"/>0</label><br />
-        <label><input type="checkbox" value="1" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-0 focus:ring-offset-0"/>1</label><br />
-        <label><input type="checkbox" value="2" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-0 focus:ring-offset-0"/>2</label><br />
+        <div className = "col1">
+          <label ><input type="checkbox" value="-1" style={{"color":colorRecode(myColor(-1)), "background-color": colorRecode(myColor(-1))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>unclustered</label><br />
+          <label ><input type="checkbox" value="0" style={{"color":colorRecode(myColor(0)), "background-color": colorRecode(myColor(0))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>0</label><br />
+          <label ><input type="checkbox" value="1" style={{"color":colorRecode(myColor(1)), "background-color": colorRecode(myColor(1))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>1</label><br />
+          <label ><input type="checkbox" value="2" style={{"color":colorRecode(myColor(2)), "background-color": colorRecode(myColor(2))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>2</label><br />
+          <label ><input type="checkbox" value="3" style={{"color":colorRecode(myColor(3)), "background-color": colorRecode(myColor(3))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>3</label><br />
+          <label ><input type="checkbox" value="4" style={{"color":colorRecode(myColor(4)), "background-color": colorRecode(myColor(4))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>4</label><br />
+          <label ><input type="checkbox" value="5" style={{"color":colorRecode(myColor(5)), "background-color": colorRecode(myColor(5))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>5</label><br />
+          <label ><input type="checkbox" value="6" style={{"color":colorRecode(myColor(6)), "background-color": colorRecode(myColor(6))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>6</label><br />
+          <label ><input type="checkbox" value="7" style={{"color":colorRecode(myColor(7)), "background-color": colorRecode(myColor(7))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>7</label><br />
+          <label ><input type="checkbox" value="8" style={{"color":colorRecode(myColor(8)), "background-color": colorRecode(myColor(8))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>8</label><br />
+        </div>
+        <div className = "col2">
+        <label ><input type="checkbox" value="9" style={{"color":colorRecode(myColor(9)), "background-color": colorRecode(myColor(9))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>9</label><br />
+        <label ><input type="checkbox" value="10" style={{"color":colorRecode(myColor(10)), "background-color": colorRecode(myColor(10))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>10</label><br />
+        <label ><input type="checkbox" value="11" style={{"color":colorRecode(myColor(11)), "background-color": colorRecode(myColor(11))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>11</label><br />
+        <label ><input type="checkbox" value="12" style={{"color":colorRecode(myColor(12)), "background-color": colorRecode(myColor(12))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>12</label><br />
+        <label ><input type="checkbox" value="13" style={{"color":colorRecode(myColor(13)), "background-color": colorRecode(myColor(13))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>13</label><br />
+        <label ><input type="checkbox" value="14" style={{"color":colorRecode(myColor(14)), "background-color": colorRecode(myColor(14))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>14</label><br />
+        <label ><input type="checkbox" value="15" style={{"color":colorRecode(myColor(15)), "background-color": colorRecode(myColor(15))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>15</label><br />
+        <label ><input type="checkbox" value="16" style={{"color":colorRecode(myColor(16)), "background-color": colorRecode(myColor(16))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>16</label><br />
+        <label ><input type="checkbox" value="17" style={{"color":colorRecode(myColor(17)), "background-color": colorRecode(myColor(17))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>17</label><br />
+        <label ><input type="checkbox" value="18" style={{"color":colorRecode(myColor(18)), "background-color": colorRecode(myColor(18))}} className={`w-4 h-4 border-gray-300 rounded focus:ring-0 focus:ring-offset-0`}/>18</label><br />
+        </div>
       </div>
       <div className="pointplot">
         <svg ref={svgRef} width="100%" height="50vh">
