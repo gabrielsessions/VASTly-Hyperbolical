18c016b93dba9a59fdb47b4bcab8726bc660d7c9
Gabriel Sessions <gsess7732@gmail.com>
2023-05-05 16:00:27 -0400

    Update AnalyticsSystem.jsx

--- Website/frontend/src/components/AnalyticsSystem.jsx
+++ Website/frontend/src/components/AnalyticsSystem.jsx
@@ -12,30 +12,16 @@ export default function AnalyticsSystem() {
   // global data variable:
   // 2 Queries one for graph/table, one for tsne
 
-
-  const [TSNEQuery, setTSNEQuery] = useState({
+  const initialQuery = {
     sqlQuery: "",
     data: [],
     fields: []
-  });
+  }
 
-  const [timelineQuery, setTimelineQuery] = useState({
-    sqlQuery: "",
-    data: [],
-    fields: []
-  });
-
-  const [tableQuery, setTableQuery] = useState({
-    sqlQuery: "",
-    data: [],
-    fields: []
-  });
-
-  const [graphQuery, setGraphQuery] = useState({
-    sqlQuery: "",
-    data: [],
-    fields: []
-  });
+  const [TSNEQuery, setTSNEQuery] = useState(initialQuery);
+  const [timelineQuery, setTimelineQuery] = useState(initialQuery);
+  const [tableQuery, setTableQuery] = useState(initialQuery);
+  const [graphQuery, setGraphQuery] = useState(initialQuery);
 
   function executeQuery(query, callback) {
     fetch('http://localhost:3001/dbtest/query?' + new URLSearchParams({
@@ -69,8 +55,8 @@ export default function AnalyticsSystem() {
 
   }
   function initialTSNEQuery() {
-    const initTSNEQuery = ""; // ADD TSNE QUERY HERE!!
-    //const initTSNEQuery = "SELECT * FROM car_data;";
+    //const initTSNEQuery = ""; // ADD TSNE QUERY HERE!!
+    const initTSNEQuery = "SELECT * FROM car_data;";
     executeQuery(initTSNEQuery, (res) => {
       const newFields = res.fields.map((e) => e.name);
       setTSNEQuery({
